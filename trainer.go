3521d46983ac8e5d0c21d9c29d518309644c8b5c
Traun Leyden <traun@couchbase.com>
2013-06-06 23:58:33 -0700

    stub for trainer

---
+++ trainer.go
@@ -0,0 +1,23 @@
+package neurgo
+
+type TrainingSample struct {
+    sampleInput []float64
+    expectedOutput []float64
+}
+
+
+type StochasticHillClimber struct {
+    currentCandidate []*Node
+    currentOptimal []*Node
+}
+
+
+
+type Trainer interface {
+	train(examples []*TrainingSample) []*Node 
+}
+
+
+func (shc *StochasticHillClimber) train(examples []*TrainingSample) []*Node {
+	return make([]*Node, 1)
+}
