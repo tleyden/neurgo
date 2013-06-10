36c0fced9e204521e4458dd17b9183479ddc60b3
Traun Leyden <traun@couchbase.com>
2013-06-10 09:25:12 -0700

    TestVectorEqualsWithMaxDelta

--- mathutil.go
+++ mathutil.go
@@ -12,3 +12,14 @@ func equalsWithMaxDelta(x, y, maxDelta float64) bool {
 	delta := math.Abs(x - y)
 	return delta <= maxDelta
 }
+
+func vectorEqualsWithMaxDelta(xValues, yValues []float64, maxDelta float64) bool {
+	equals := true
+	for i, x := range xValues {
+		y := yValues[i]
+		if !equalsWithMaxDelta(x, y, maxDelta) {
+			equals = false
+		}
+	}
+	return equals
+}
--- mathutil_test.go
+++ mathutil_test.go
@@ -17,3 +17,17 @@ func TestEqualsWithMaxDelta(t *testing.T) {
 	assert.True(t, equalsWithMaxDelta(0.99999, 1.00000, .01))
 	assert.False(t, equalsWithMaxDelta(0.95, 1.00000, .01))
 }
+
+
+func TestVectorEqualsWithMaxDelta(t *testing.T) {
+	
+	xValues := []float64{0.99999, 0.00000}
+	yValues := []float64{1.00000, 0.00001}
+
+	assert.True(t, vectorEqualsWithMaxDelta(xValues, yValues, .01))
+
+	xValues = []float64{0.95, 0.00000}
+	yValues = []float64{1.00000, 1.00000}
+
+	assert.False(t, vectorEqualsWithMaxDelta(xValues, yValues, .01))
+}
