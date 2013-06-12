4d29780fe04d2eaa1f01021a0a4074347731cdf0
Traun Leyden <traun@couchbase.com>
2013-06-11 19:07:05 -0700

    refactor more tests to use neural net verify

--- neural_network_test.go
+++ neural_network_test.go
@@ -3,7 +3,6 @@ package neurgo
 import (
 	"testing"
 	"github.com/couchbaselabs/go.assert"
-	"sync"
 	"log"
 )
 
@@ -41,8 +40,6 @@ func TestNetwork(t *testing.T) {
 	verified := neuralNet.Verify(examples)
 	assert.True(t, verified)
 
-	log.Printf("Done")
-
 
 }
 
@@ -57,9 +54,6 @@ func TestXnorNetwork(t *testing.T) {
 	sensor1 := &Sensor{}
 	sensor2 := &Sensor{}
 	actuator := &Actuator{}
-	wiretap := &Wiretap{}
-	injector1 := &Injector{}
-	injector2 := &Injector{}
 
 	// give names to network nodes
 	sensor1.Name = "sensor1"
@@ -72,8 +66,6 @@ func TestXnorNetwork(t *testing.T) {
 	actuator.Name = "actuator"
 
 	// connect nodes together 
-	injector1.ConnectBidirectional(sensor1)
-	injector2.ConnectBidirectional(sensor2)
 	sensor1.ConnectBidirectionalWeighted(input_neuron1, []float64{1})
 	sensor2.ConnectBidirectionalWeighted(input_neuron2, []float64{1})
 	input_neuron1.ConnectBidirectionalWeighted(hidden_neuron1, []float64{20})
@@ -83,7 +75,21 @@ func TestXnorNetwork(t *testing.T) {
 	hidden_neuron1.ConnectBidirectionalWeighted(output_neuron, []float64{20})
 	hidden_neuron2.ConnectBidirectionalWeighted(output_neuron, []float64{20})
 	output_neuron.ConnectBidirectional(actuator)
-	actuator.ConnectBidirectional(wiretap)
+
+	// create neural network
+	sensors := []*Sensor{sensor1, sensor2}	
+	actuators := []*Actuator{actuator}
+	neuralNet := &NeuralNetwork{sensors: sensors, actuators: actuators}
+
+	// inputs + expected outputs
+	examples := []*TrainingSample{
+
+		// TODO: how to wrap this?
+		{sampleInputs: [][]float64{[]float64{0},[]float64{1}}, expectedOutputs: [][]float64{[]float64{0}}},
+		{sampleInputs: [][]float64{[]float64{1},[]float64{1}}, expectedOutputs: [][]float64{[]float64{1}}},
+		{sampleInputs: [][]float64{[]float64{1},[]float64{0}}, expectedOutputs: [][]float64{[]float64{0}}},
+		{sampleInputs: [][]float64{[]float64{0},[]float64{0}}, expectedOutputs: [][]float64{[]float64{1}}}}
+
 
 	// spinup node goroutines
 	signallers := []Signaller{input_neuron1, input_neuron2, hidden_neuron1, hidden_neuron2, output_neuron, sensor1, sensor2, actuator}
@@ -91,46 +97,10 @@ func TestXnorNetwork(t *testing.T) {
 		go Run(signaller)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Add(1)
+	// verify neural network
+	verified := neuralNet.Verify(examples)
+	assert.True(t, verified)
 
-	testInputs := []struct{
-		x1 []float64
-		x2 []float64
-	}{ {x1: []float64{0}, x2: []float64{1}},
-		{x1: []float64{1}, x2: []float64{1}},
-		{x1: []float64{1}, x2: []float64{0}},
-		{x1: []float64{0}, x2: []float64{0}}}
-
-	expectedOutputs := []float64{ 0.0000, 1.00000, 0.0000, 1.00000 }
-
-	// inject a value into sensor
-	go func() {
-
-		for _, inputPair := range testInputs {
-			injector1.outbound[0].channel <- inputPair.x1
-			injector2.outbound[0].channel <- inputPair.x2
-		}
-
-		wg.Done()
-	}()
-
-	// read the value from wiretap (which taps into actuator)
-	go func() {
-		
-		for _, expectedOuput := range expectedOutputs {
-			resultVector := <- wiretap.inbound[0].channel
-			result := resultVector[0]
-			assert.True(t, equalsWithMaxDelta(result, expectedOuput, .01))
-		}
-
-		wg.Done() 
-	}()
-
-	wg.Wait()
-
-	log.Printf("Done")
 
 }
 
