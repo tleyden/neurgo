aa4906f50244ad6b5c8e97c08d26bceb13436190
Traun Leyden <traun@couchbase.com>
2013-06-16 20:37:43 -0700

    pseudocode for trainer implementation

--- trainer.go
+++ trainer.go
@@ -19,6 +19,34 @@ type Trainer interface {
 	train(neuralNet *NeuralNetwork, examples []*TrainingSample) *NeuralNetwork 
 }
 
-func (shc *StochasticHillClimber) train(examples []*TrainingSample) *NeuralNetwork {
+func (shc *StochasticHillClimber) train(neuralNet *NeuralNetwork, examples []*TrainingSample) *NeuralNetwork {
+
+	/*
+	// Repeat
+
+	// Apply NN to problem and save fitness
+	fitness = neuralNet.FitnessScore(examples)
+	
+	// Save the genotype
+	currentWinner := neuralNet.Copy()
+
+	// Perturb synaptic weights and biases
+	shc.PerturbParameters(neuralNet)
+
+	// Re-Apply NN to problem
+	newFitness := neuralNet.FitnessScore(examples)
+
+	// If the fitness of the perturbed NN is higher, discard original NN and keep 
+	// the new.  If the fitness of original is higher, discard perturbed and keep
+	// the old.
+	if newFitness < fitness {
+		neuralNet.RestoreFrom(currentWinner)
+	}
+
+	// Until - acceptable solution is found, or stopping condition reached
+
+	// Return - genotype with the fittest combination of weights
+        */
+
 	return &NeuralNetwork{}
 }
