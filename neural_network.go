ae432534c6c14b4eee79bd521f3702f8d4249c99
Traun Leyden <traun@couchbase.com>
2013-06-16 20:40:41 -0700

    begin implementation of nn copy method.  still in progress.

--- neural_network.go
+++ neural_network.go
@@ -3,6 +3,7 @@ package neurgo
 import (
 	"sync"
 	"fmt"
+	"log"
 )
 
 type NeuralNetwork struct {
@@ -79,7 +80,96 @@ func (neuralNet *NeuralNetwork) Verify(samples []*TrainingSample) bool {
 		actuator.DisconnectBidirectional(wiretaps[i])
 	}
 
-
 	return verified  
+
+}
+
+func (neuralNet *NeuralNetwork) Copy() *NeuralNetwork {
+
+	
+	// create an empty map between old nodes and new nodes
+	// var nodeScaffold map[*Node]*Node
+	nodeScaffold := make(map[Connector]Connector)
+
+	sensorsCopy := make([]*Sensor,0)
+	actuatorsCopy := make([]*Actuator, 0)
+	neuralNetCopy := &NeuralNetwork{sensors: sensorsCopy, actuators: actuatorsCopy}
+
+	for _, sensor := range neuralNet.sensors {
+		sensorCopy := new(Sensor)
+		nodeScaffold[sensor] = sensorCopy
+		sensorCopy.Name = sensor.Name
+		neuralNetCopy.sensors = append(neuralNetCopy.sensors, sensorCopy)
+		log.Printf("sensorOriginal: %v", sensor)
+		log.Printf("sensorCopy: %v", sensorCopy)
+
+		recreateOutboundConnectionsRecursive(sensor, sensorCopy, nodeScaffold)
+	}
+
+	return neuralNetCopy
+
+}
+
+
+func recreateOutboundConnectionsRecursive(nodeOriginal Connector, nodeCopy Connector, nodeScaffold map[Connector]Connector) {
+	
+	// for each outbound connection:
+	//   see if connection target node copy exists in map, if not, create it
+	//   duplicate the outgoing connection
+	//   maybe: duplicate the incoming connection on the other side of this outbound 
+	//          (or maybe this makes sense to do in a separate pass)
+	//   recursive call for the connection target
+	
+	var cxnTargetCopy Connector
+	for _, outboundConnection := range nodeOriginal.outboundConnections() {
+
+		cxnTargetOriginal := outboundConnection.other
+		
+		if cxnTargetCopyTemp, ok := nodeScaffold[cxnTargetOriginal]; ok {  // TODO: hack
+			cxnTargetCopy = cxnTargetCopyTemp
+		} else {
+
+			// the connection target does not exist in nodeScaffold, create it
+			switch t:= cxnTargetOriginal.(type) {
+			case *Sensor:
+				log.Printf("its a sensor: %T", t)
+				sensor := &Sensor{}
+				sensor.Name = t.Name
+				cxnTargetCopy = sensor
+			case *Neuron:
+				log.Printf("its a neuron: %T", t)
+				neuron := &Neuron{}
+				neuron.Name = t.Name
+				cxnTargetCopy = neuron
+			case *Actuator:
+				log.Printf("its an actuator: %T", t)
+				actuator := &Actuator{}
+				actuator.Name = t.Name
+				cxnTargetCopy = actuator
+			default:
+
+				msg := fmt.Sprintf("unexpected cxnTargetOriginal type: %T", t) 
+				panic(msg)
+			}
+			nodeScaffold[cxnTargetOriginal] = cxnTargetCopy
+
+		}
+
+		log.Printf("map: %v", nodeScaffold)
+
+		newConnection := &connection{}
+		newConnection.other = cxnTargetCopy
+
+		if len(cxnTargetOriginal.outboundConnections()) > 0 {
+			log.Printf("recursing into recreateOutboundConnectionsRecursive with: %v, %v, %v ", cxnTargetOriginal, cxnTargetCopy, nodeScaffold)
+			recreateOutboundConnectionsRecursive(cxnTargetOriginal, cxnTargetCopy, nodeScaffold)
+		} else {
+			log.Printf("not recursing, cxnTargetOriginal has no outbound connections")
+		}
+		
+
+	} 
+
+
 
 }
