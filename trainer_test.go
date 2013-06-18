beb771936a9450511a473998b6bb1f97b06b1b35
Traun Leyden <traun@couchbase.com>
2013-06-18 08:38:29 -0700

    trainer test now compiles .. in last commit I said the tests were passing, was probably not true.  they just compiled.

--- trainer_test.go
+++ trainer_test.go
@@ -9,19 +9,19 @@ import (
 // create netwwork with topology capable of solving XNOR, but which 
 // has not been trained yet
 func xnorNetworkUntrained() *NeuralNetwork {
-	
-	hidden_neuron1 := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
-	hidden_neuron2 := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
-	output_neuron := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
-	sensor := &Sensor{}
-	actuator := &Actuator{}
 
-	// give names to network nodes
-	sensor.Name = "sensor"
-	hidden_neuron1.Name = "hidden_neuron1"
-	hidden_neuron2.Name = "hidden_neuron2"
-	output_neuron.Name = "output_neuron"
-	actuator.Name = "actuator"
+	// create network nodes
+	hn1_processor := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
+	hidden_neuron1 := &Node{Name: "hidden_neuron1", processor: hn1_processor}
+	
+	hn2_processor := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
+	hidden_neuron2 := &Node{Name: "hidden_neuron2", processor: hn2_processor}
+
+	outn_processor := &Neuron{Bias: 0, ActivationFunction: sigmoid}  
+	output_neuron := &Node{Name: "output_neuron", processor: outn_processor}
+
+	sensor := &Node{Name: "sensor", processor: &Sensor{}}
+	actuator := &Node{Name: "actuator", processor: &Actuator{}}
 
 	// connect nodes together 
 	sensor.ConnectBidirectionalWeighted(hidden_neuron1, []float64{0, 0})
@@ -31,14 +31,14 @@ func xnorNetworkUntrained() *NeuralNetwork {
 	output_neuron.ConnectBidirectional(actuator)
 
 	// create neural network
-	sensors := []*Sensor{sensor}	
-	actuators := []*Actuator{actuator}
+	sensors := []*Node{sensor}	
+	actuators := []*Node{actuator}
 	neuralNet := &NeuralNetwork{sensors: sensors, actuators: actuators}
 
 	// spinup node goroutines
-	signallers := []Connector{sensor, hidden_neuron1, hidden_neuron2, output_neuron, actuator}
-	for _, signaller := range signallers {
-		go Run(signaller)
+	nodes := []*Node{sensor, hidden_neuron1, hidden_neuron2, output_neuron, actuator}
+	for _, node := range nodes {
+		go Run(node.processor, node)
 	}
 	
 	return neuralNet
