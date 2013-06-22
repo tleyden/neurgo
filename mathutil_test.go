06c52d98043e39f68487dbfa3f13de80989fc33e
Traun Leyden <traun@couchbase.com>
2013-06-22 15:35:21 -0700

    formatting

--- mathutil_test.go
+++ mathutil_test.go
@@ -6,11 +6,9 @@ import (
 )
 
 func TestSigmoid(t *testing.T) {
-
 	assert.Equals(t, sigmoid(0), 0.5)
 	assert.True(t, sigmoid(10) > 0.9)
 	assert.True(t, sigmoid(-10) < 0.1)
-
 }
 
 func TestEqualsWithMaxDelta(t *testing.T) {
