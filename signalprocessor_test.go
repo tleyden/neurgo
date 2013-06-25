6674d341b6d3278d7772eca2cdccbdff8104a17b
Traun Leyden <traun@couchbase.com>
2013-06-25 07:43:42 -0700

    add test that detects bug when re-adding connection to running node with no connections

--- signalprocessor_test.go
+++ signalprocessor_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestCanPropagateSignal(t *testing.T) {
+func TestCanPropagateSignalShutdown(t *testing.T) {
 
 	// create nodes
 	neuronProcessor1 := &Neuron{Bias: 10, ActivationFunction: identity_activation}
@@ -18,12 +18,6 @@ func TestCanPropagateSignal(t *testing.T) {
 	actuator := &Node{Name: "actuator", processor: actuatorProcessor}
 
 	actuator.Run()
-
-	// this hack is needed ... I think because the closing channels
-	// aren't setup yet and it breaks things.  TODO: modify
-	// node.Run() to be synchronous and kick off go routine internally
-	// after closing channels have been setup
-	// time.Sleep(time.Second / 100)
 
 	// connect nodes together
 	neuron1.ConnectBidirectional(actuator)
@@ -48,6 +42,44 @@ func TestCanPropagateSignal(t *testing.T) {
 	isShutdown = actuator.processor.waitCanPropagate(actuator)
 	log.Printf("called waitCanPropagate() and got isShutdown: %v", isShutdown)
 	assert.True(t, isShutdown)
+
+	log.Printf("done")
+
+}
+
+func TestCanPropagateSignalReAddConnection(t *testing.T) {
+
+	// create nodes
+	neuronProcessor1 := &Neuron{Bias: 10, ActivationFunction: identity_activation}
+	neuronProcessor2 := &Neuron{Bias: 10, ActivationFunction: identity_activation}
+	neuron1 := &Node{Name: "neuron1", processor: neuronProcessor1}
+	neuron2 := &Node{Name: "neuron2", processor: neuronProcessor2}
+	actuatorProcessor := &Actuator{}
+	actuator := &Node{Name: "actuator", processor: actuatorProcessor}
+
+	actuator.Run()
+
+	// connect nodes together
+	neuron1.ConnectBidirectional(actuator)
+	neuron2.ConnectBidirectional(actuator)
+
+	isShutdown := actuator.processor.waitCanPropagate(actuator)
+	assert.False(t, isShutdown)
+
+	go func() {
+		log.Printf("goroutine sleeping")
+		time.Sleep(time.Second / 100)
+		neuron1.ConnectBidirectional(actuator)
+	}()
+
+	log.Printf("-- disconnect nodes from actuator")
+	neuron1.DisconnectBidirectional(actuator)
+	neuron2.DisconnectBidirectional(actuator)
+
+	log.Printf("-- call waitCanPropagate()")
+	isShutdown = actuator.processor.waitCanPropagate(actuator)
+	log.Printf("called waitCanPropagate() and got isShutdown: %v", isShutdown)
+	assert.False(t, isShutdown)
 
 	log.Printf("done")
 
