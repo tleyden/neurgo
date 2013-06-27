2964c355300febe20dc315177739c0cb104eed28
Traun Leyden <traun@couchbase.com>
2013-06-27 08:04:38 -0700

    delete a few tests that are no longer relevant

--- node_test.go
+++ node_test.go
@@ -3,7 +3,6 @@ package neurgo
 import (
 	"github.com/couchbaselabs/go.assert"
 	"log"
-	"sync"
 	"testing"
 	"time"
 )
@@ -85,95 +84,6 @@ func TestRemoveConnection(t *testing.T) {
 	assert.Equals(t, len(neuron2.inbound), 1)
 	assert.Equals(t, len(sensor.outbound), 1)
 	assert.Equals(t, sensor.outbound[0].channel, neuron2.inbound[0].channel)
-
-}
-
-func TestRemoveConnectionFromRunningNeuron(t *testing.T) {
-
-	// create nodes
-	sensor1 := &Node{Name: "sensor1", processor: &Sensor{}}
-	sensor2 := &Node{Name: "sensor2", processor: &Sensor{}}
-	neuronProcessor := &Neuron{Bias: 10, ActivationFunction: identity_activation}
-	neuron := &Node{Name: "neuron", processor: neuronProcessor}
-
-	// connect nodes together
-	weights := []float64{20}
-	sensor1.ConnectBidirectionalWeighted(neuron, weights)
-	sensor2.ConnectBidirectionalWeighted(neuron, weights)
-
-	// basic sanity check, send two inputs to neuron inbound channels
-	// and verify that weightedInputs() returns both inputs
-	go func() {
-		sensor1.outbound[0].channel <- []float64{0}
-	}()
-	go func() {
-		sensor2.outbound[0].channel <- []float64{0}
-	}()
-	weightedInputs, _ := neuronProcessor.weightedInputs(neuron)
-	assert.Equals(t, len(weightedInputs), 2)
-
-	// close one channel while a neuron is reading from
-	// both inbound connections, make sure it returns one value
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Add(1)
-
-	go func() {
-		weightedInputs, _ := neuronProcessor.weightedInputs(neuron)
-		assert.Equals(t, len(weightedInputs), 1)
-		wg.Done()
-	}()
-
-	go func() {
-
-		// need to sleep so that we can be sure that the other go-routine
-		// is blocked on the channel read of its inbound channels
-		time.Sleep(0.1 * 1e9)
-
-		sensor1.DisconnectBidirectional(neuron)
-		sensor2.outbound[0].channel <- []float64{0}
-		wg.Done()
-	}()
-
-	wg.Wait()
-}
-
-func TestRemoveConnectionFromRunningActuator(t *testing.T) {
-
-	// create nodes
-	neuronProcessor1 := &Neuron{Bias: 10, ActivationFunction: identity_activation}
-	neuronProcessor2 := &Neuron{Bias: 10, ActivationFunction: identity_activation}
-	neuron1 := &Node{Name: "neuron1", processor: neuronProcessor1}
-	neuron2 := &Node{Name: "neuron2", processor: neuronProcessor2}
-	actuatorProcessor := &Actuator{}
-	actuator := &Node{Name: "actuator", processor: actuatorProcessor}
-
-	// connect nodes together
-	neuron1.ConnectBidirectional(actuator)
-	neuron2.ConnectBidirectional(actuator)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Add(1)
-
-	go func() {
-		inputs, _ := actuatorProcessor.gatherInputs(actuator)
-		assert.Equals(t, len(inputs), 1)
-		wg.Done()
-	}()
-
-	go func() {
-
-		// need to sleep so that we can be sure that the other go-routine
-		// is blocked on the channel read of its inbound channels
-		time.Sleep(0.1 * 1e9)
-
-		neuron1.DisconnectBidirectional(actuator)
-		neuron2.outbound[0].channel <- []float64{0}
-		wg.Done()
-	}()
-
-	wg.Wait()
 
 }
 
