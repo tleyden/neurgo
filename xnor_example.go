8a75259e4f4537e2b2bd68be4b681744d4725272
Traun Leyden <traun@couchbase.com>
2013-11-04 07:39:03 -0800

    refactor XnorCortexUntrained from neurvolve to neurgo

--- xnor_example.go
+++ xnor_example.go
@@ -69,6 +69,76 @@ func XnorCortex() *Cortex {
 
 }
 
+func XnorCortexUntrained() *Cortex {
+
+	sensorNodeId := NewSensorId("sensor", 0.0)
+	hiddenNeuron1NodeId := NewNeuronId("hidden-neuron1", 0.25)
+	hiddenNeuron2NodeId := NewNeuronId("hidden-neuron2", 0.25)
+	outputNeuronNodeIde := NewNeuronId("output-neuron", 0.35)
+
+	actuatorNodeId := NewActuatorId("actuator", 0.5)
+
+	hiddenNeuron1 := &Neuron{
+		ActivationFunction: EncodableSigmoid(),
+		NodeId:             hiddenNeuron1NodeId,
+		Bias:               RandomBias(),
+	}
+	hiddenNeuron1.Init()
+
+	hiddenNeuron2 := &Neuron{
+		ActivationFunction: EncodableSigmoid(),
+		NodeId:             hiddenNeuron2NodeId,
+		Bias:               RandomBias(),
+	}
+	hiddenNeuron2.Init()
+
+	outputNeuron := &Neuron{
+		ActivationFunction: EncodableSigmoid(),
+		NodeId:             outputNeuronNodeIde,
+		Bias:               RandomBias(),
+	}
+	outputNeuron.Init()
+
+	sensor := &Sensor{
+		NodeId:       sensorNodeId,
+		VectorLength: 2,
+	}
+	sensor.Init()
+
+	actuator := &Actuator{
+		NodeId:       actuatorNodeId,
+		VectorLength: 1,
+	}
+	actuator.Init()
+
+	sensor.ConnectOutbound(hiddenNeuron1)
+	hiddenNeuron1.ConnectInboundWeighted(sensor, RandomWeights(2))
+
+	sensor.ConnectOutbound(hiddenNeuron2)
+	hiddenNeuron2.ConnectInboundWeighted(sensor, RandomWeights(2))
+
+	hiddenNeuron1.ConnectOutbound(outputNeuron)
+	outputNeuron.ConnectInboundWeighted(hiddenNeuron1, RandomWeights(1))
+
+	hiddenNeuron2.ConnectOutbound(outputNeuron)
+	outputNeuron.ConnectInboundWeighted(hiddenNeuron2, RandomWeights(1))
+
+	outputNeuron.ConnectOutbound(actuator)
+	actuator.ConnectInbound(outputNeuron)
+
+	nodeId := NewCortexId("cortex")
+
+	cortex := &Cortex{
+		NodeId:    nodeId,
+		Sensors:   []*Sensor{sensor},
+		Neurons:   []*Neuron{hiddenNeuron1, hiddenNeuron2, outputNeuron},
+		Actuators: []*Actuator{actuator},
+	}
+
+	return cortex
+
+}
+
 func XnorTrainingSamples() []*TrainingSample {
 
 	// inputs + expected outputs
