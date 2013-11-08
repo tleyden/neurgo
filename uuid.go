baec3ee526a2aa8c8c894e71f3edf6fbc1c54b02
Traun Leyden <traun@couchbase.com>
2013-11-08 08:09:07 -0800

    take todo= out of uuid

--- uuid.go
+++ uuid.go
@@ -7,5 +7,5 @@ import (
 func NewUuid() string {
 	// TODO: do a real uuid - https://github.com/nu7hatch/gouuid
 	randInt := RandomIntInRange(0, 10000000000)
-	return fmt.Sprintf("todo=%v", randInt)
+	return fmt.Sprintf("%d", randInt)
 }
