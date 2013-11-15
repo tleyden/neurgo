20d1646817e51374ae9072ccb9333f78490948ae
Traun Leyden <traun@couchbase.com>
2013-11-15 09:23:43 -0800

    format the json w/ indentation

--- nnviewer/nnviewer.go
+++ nnviewer/nnviewer.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/couchbaselabs/logg"
 	ng "github.com/tleyden/neurgo"
 	"os"
@@ -13,12 +15,28 @@ func init() {
 func main() {
 
 	pathToCortex := os.Args[1]
-	logg.LogTo("DEBUG", "Opening file: %v", pathToCortex)
+	cortex := unmarshalCortex(pathToCortex)
+	renderSVG(cortex)
+	dumpCompactDescription(cortex)
+	prettyPrintJSON(cortex)
+
+}
+
+func unmarshalCortex(pathToCortex string) *ng.Cortex {
 
 	cortex, err := ng.NewCortexFromJSONFile(pathToCortex)
 	if err != nil {
 		logg.LogPanic("Error reading cortex from: %v.  Err: %v", pathToCortex, err)
 	}
+	return cortex
+
+}
+
+func dumpCompactDescription(cortex *ng.Cortex) {
+	logg.LogTo("DEBUG", "Compact: %v", cortex.StringCompact())
+}
+
+func renderSVG(cortex *ng.Cortex) {
 
 	filename := "out.svg"
 	outfile, err := os.Create(filename)
@@ -33,5 +51,16 @@ func main() {
 
 	cortex.RenderSVG(outfile)
 	logg.LogTo("DEBUG", "svg available here: %v", filename)
+
+}
+
+func prettyPrintJSON(cortex *ng.Cortex) {
+
+	json, err := json.MarshalIndent(cortex, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	jsonString := fmt.Sprintf("%s", json)
+	logg.LogTo("DEBUG", "%v", jsonString)
 
 }
